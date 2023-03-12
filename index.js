aa9227312474c168899cdaddcd787e9bccf6a759
NatWil05 <williams.natasha09@gmail.com>
2023-03-12 16:38:24 -0400

    completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield",]
+    function destructivelyAppendCat(name) {
+        cats.push("Ralph")
+    }
+    function destructivelyPrependCat(name) {
+        cats.unshift("Bob")
+    }
+    function destructivelyRemoveLastCat(name) {
+        cats.pop("Ralph")
+    }
+    function destructivelyRemoveFirstCat(name) {
+        cats.shift("Bob")
+    }
+
+    function appendCat(name) {
+        const newCats = [...cats]
+        newCats.push("Broom")
+        return newCats
+    }
+    function prependCat(name) {
+        const newCats = [...cats]
+        newCats.unshift("Arnold")
+        return newCats
+    }
+
+    function removeLastCat() {
+        const copyOfCats = cats.slice()
+        copyOfCats.pop()
+        return copyOfCats
+    }
+
+    function removeFirstCat() {
+        const copyOfCats = cats.slice()
+        copyOfCats.shift()
+        return copyOfCats
+    }
